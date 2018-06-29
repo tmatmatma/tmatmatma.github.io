492dec6da32ceb710c539e3bf60a0e5ff183bce0
Tracy Ma <tma523@gmail.com>
2018-06-28 22:01:20 -0400

    this

---
+++ _assets/js/script.js
@@ -0,0 +1,138 @@
+
+// var images = new Array();
+
+// var parentId = $('.main-img').parent('div').attr('id');
+
+$( document ).ready(function() {
+
+
+					$("div").error(function(){
+					        alert('ERROR!');
+					});
+    // console.log( "ready!" );
+    console.log('ready');
+	//var displayImage = $('.main-img').parent('div').attr('id');
+
+	$(".main-img").each(function() {
+		// var i;
+		// for (var i = 1; i <= 20; i++) {
+			$(this).css('background-image', 'url(' + '_assets/img/' + $(this).parent('div').attr('id') + '/img1' + '.jpg' + ')');
+
+		// }
+	});
+
+
+    // $(".main-img").css('background-image', 'url(' + '_assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
+
+    var count=0
+
+
+
+    count = 0;
+
+
+
+
+  //   $.ajax({
+  // url: '_assets/img/' + displayImage + '/img' + count + '.jpg',
+  // success: function(data){
+
+    	$(".main-img").on('click', function() {
+
+
+
+    		//if (count < 5) {
+    		
+    		    var displayImage = $(this).parent('div').attr('id');
+
+$.ajax({
+    url:'_assets/img/' + displayImage + '/img' + count + '.jpg',
+    type:'HEAD',
+    error: function()
+    {
+        count = 1;
+    },
+    success: function()
+    {
+        count++;
+        $(this).css('background-image', 'url(' + '_assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
+    }
+});
+
+
+
+    		    // $(this).css('background-image', 'url(' + '_assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
+    	
+
+    		   // } else {
+    		   // 	count = 0;
+    		   // }
+
+
+    		   //  var numofImgs =  $.ajax({
+    					// 			url:'_assets/img/'+displayImage+'_title.jpg',
+   						// 			type:'HEAD',
+								 //    error: function()
+								 //    { 
+								 //      //file doesn't exist
+								 //    },
+								 //    success: function()
+								 //    {
+								 //        count++;
+								 //    }
+									// });
+
+
+// $.ajax({
+//     url:'_assets/img/' + displayImage + '/img' + count + '.jpg',
+//     type:'HEAD',
+//     error: function()
+//     { 
+//       alert('EMPTY FOLDER');
+//     },
+//     success: function()
+//     {
+//         count++;
+//     		$(this).css('background-image', 'url(' + '_assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
+//     }
+// });
+
+
+
+
+
+
+
+
+	// console.log("help");
+	// console.log( $(this).parent('div').attr('id') );	
+		//if(count===10){count=1}
+
+});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+// var url = "_assets/img/" + parentId;
+// var fileextension = ".png";
+
+// $.ajax({
+//     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+//     url: dir,
+//     success: function (data) {
+//         //List all .png file names in the page
+//         $(data).find("a:contains(" + fileextension + ")").each(function () {
+//             var filename = this.href.replace(window.location.host, "").replace("http://", "");
+//             $("body").append("<img src='" + dir + filename + "'>");
+//         });
+//     }
+// });
