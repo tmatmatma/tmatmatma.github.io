4377d89d47b471211617bd6c67070a136b216c1a
Tracy Ma <tma523@gmail.com>
2018-06-28 22:21:08 -0400

    reverted js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,17 @@
 
-// var images = new Array();
-
-// var parentId = $('.main-img').parent('div').attr('id');
 
 $( document ).ready(function() {
 
 
-					// $("div").error(function(){
-					//         alert('ERROR!');
-					// });
-    // console.log( "ready!" );
     console.log('ready');
-	//var displayImage = $('.main-img').parent('div').attr('id');
-
 	$(".main-img").each(function() {
-		// var i;
-		// for (var i = 1; i <= 20; i++) {
 			$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1' + '.jpg' + ')');
-
-		// }
 	});
 
 
-    // $(".main-img").css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
-
-    var count=1
 
 
-
-    // count = 1;
-
-
-
-
-  //   $.ajax({
-  // url: 'assets/img/' + displayImage + '/img' + count + '.jpg',
-  // success: function(data){
+    count = 0;
 
     	$(".main-img").on('click', function() {
 
@@ -45,70 +21,17 @@ $( document ).ready(function() {
     		
     		    var displayImage = $(this).parent('div').attr('id');
 
-// $.ajax({
-//     url:'assets/img/' + displayImage + '/img' + count + '.jpg',
-//     type:'HEAD',
-//     error: function()
-//     {
-//         count = 1;
-//     },
-//     success: function()
-//     {
-//         count++;
-//         $(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
-//     }
-// });
 
 
 
-    		     $(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
-    	
-
-    		    } else {
-    		   	count = 1;
-    		   }
-
-
-    		   //  var numofImgs =  $.ajax({
-    					// 			url:'assets/img/'+displayImage+'_title.jpg',
-   						// 			type:'HEAD',
-								 //    error: function()
-								 //    { 
-								 //      //file doesn't exist
-								 //    },
-								 //    success: function()
-								 //    {
-								 //        count++;
-								 //    }
-									// });
-
-
-// $.ajax({
-//     url:'assets/img/' + displayImage + '/img' + count + '.jpg',
-//     type:'HEAD',
-//     error: function()
-//     { 
-//       alert('EMPTY FOLDER');
-//     },
-//     success: function()
-//     {
-//         count++;
-//     		$(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
-//     }
-// });
-
-
-
-
-
-
-
-
-	// console.log("help");
-	// console.log( $(this).parent('div').attr('id') );	
-		//if(count===10){count=1}
-
+        count++;
+        $(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
+    } else {
+    	count = 0;
+    }
 });
+
+
 
 
 });
