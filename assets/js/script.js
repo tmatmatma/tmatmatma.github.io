f848a0c454cb647031392c7e3c5dd6f0cca6065f
Tracy Ma <tma523@gmail.com>
2018-06-29 19:22:46 -0400

    testing new str

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,85 @@
 
 $( document ).ready(function() {
 
+					// function increment_last(v) {
+					//     return v.replace(/[0-9]+(?!.*[0-9])/, function(match) {
+					//         return parseInt(match, 10)+1;
+					        
+					//     });
+					// }
+					// console.log(increment_last);
 
 
     console.log('okay');
 	$(".main-img").each(function() {
 			$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
+	
+
 	});
 
 
 
-	count = 1;
-    	$(".main-img").on('click', function() {
-    		
-    		count++;
-    		var displayImage = $(this).parent('div').attr('id');
 
-    		$(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
-    		 
 
-			$.get('assets/img/' + displayImage + '/img' + count + '.jpg')
+
+	//count = 1;
+$(".main-img").on('click', function() {
+	var displayImage = $(this).parent('div').attr('id');
+
+	var str = $(this).css('background-image');
+
+	str = str.replace(/[0-9]+(?!.*[0-9])/, parseInt(str.match(/[0-9]+(?!.*[0-9])/), 10)+1);
+
+	console.log(str);
+
+// function getLastNumberOfString(str){
+//   var allNumbers = str.replace(/[^0-9]/g, ' ').trim().split(/\s+/);
+//   return parseInt(allNumbers[allNumbers.length - 1], 10);
+// }
+
+	//var v = "img1.jpg"
+	//var v = str;
+	//var newNum = v+1;
+
+
+	
+	$(this).css('background-image', str);
+
+			$.get(str)
 			.success(function(d){
 			    console.log('yay');
 			})
 			.error(function(d){
-			    count = 1;
+			    //count = 1;
 			    $(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img1.jpg' + ')');
 			});
+
+
+    //$(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
+
+
+
+	  
+ 
+
+
+    	// $(".main-img").on('click', function() {
+    	// 	var lastNumber = this.match(/\d+$/);
+    	// 	console.log(lastNumber);
+    	// 	//count++;
+    	// 	var displayImage = $(this).parent('div').attr('id');
+
+    	// 	$(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img' + count + '.jpg' + ')');
+    		 
+
+			// $.get('assets/img/' + displayImage + '/img' + count + '.jpg')
+			// .success(function(d){
+			//     console.log('yay');
+			// })
+			// .error(function(d){
+			//     count = 1;
+			//     $(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img1.jpg' + ')');
+			// });
 
     		//if (count < 5) {
           
