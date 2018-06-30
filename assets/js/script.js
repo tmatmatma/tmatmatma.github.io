b8c9513696baf5bd0aa0d50471c6461f0c1ea8d9
Tracy Ma <tma523@gmail.com>
2018-06-29 23:47:23 -0400

    readyyyy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 
 
 $(document).ready(function() {
-    console.log('ready');
+    console.log('readyyyy');
 	$(".main-img").each(function() {
-		if ($(this).text().toLowerCase().match(/\.(jpg)/g)) {
-        		$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
-    	} else if ($(this).text().toLowerCase().match(/\.(gif)/g)) {
-    			$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.gif' + ')');
-    	}
+		$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
+	});
+	$(".main-gif").each(function() {
+		$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.gif' + ')');
 	});
 	
-	$(".main-img").on('click', function() {
+	$(".main-img").add(".main-gif").on('click', function() {
 		var displayImage = $(this).parent('div').attr('id');
 		var str = $(this).css('background-image');
 		str = str.replace(/[0-9]+(?!.*[0-9])/, parseInt(str.match(/[0-9]+(?!.*[0-9])/), 10)+1);
@@ -27,6 +26,9 @@ $(document).ready(function() {
     				if(xhr.status==404) {
         			$(".main-img").each(function() {
 						$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
+					});
+					$(".main-gif").each(function() {
+						$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.gif' + ')');
 					});
     			}
 		    }
