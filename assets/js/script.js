cb5b307158df1dfc2894999b9f58628c927c16ee
Tracy Ma <tma523@gmail.com>
2018-06-29 22:21:11 -0400

    lose hope

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 
 
-    console.log('helppppppp');
+    console.log('help');
 	$(".main-img").each(function() {
 			$(this).css('background-image', 'url(' + 'assets/img/' + $(this).parent('div').attr('id') + '/img1.jpg' + ')');
 	
@@ -34,6 +34,7 @@ $(document).ready(function() {
 
 
 	$(".main-img").on('click', function() {
+		var revert = $(this).css('background-image', 'url(' + 'assets/img/' + displayImage + '/img1.jpg' + ')');
 		var displayImage = $(this).parent('div').attr('id');
 		var str = $(this).css('background-image');
 		str = str.replace(/[0-9]+(?!.*[0-9])/, parseInt(str.match(/[0-9]+(?!.*[0-9])/), 10)+1);
@@ -52,8 +53,9 @@ $(document).ready(function() {
 		    	},
 				error:function (xhr, ajaxOptions, thrownError){
     				if(xhr.status==404) {
-    					console.log(this);
-        		$(".main-img").css('background-image', 'url(' + 'assets/img/' + displayImage + '/img1.jpg' + ')');
+    					console.log(revert);
+    					revert; 
+        		//$(".main-img").css('background-image', 'url(' + 'assets/img/' + displayImage + '/img1.jpg' + ')');
 
     			}
 
