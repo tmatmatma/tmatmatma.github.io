91278ba05feeb215b2c94ccbb830006651b36621
Tracy Ma <tma523@gmail.com>
2020-05-27 21:31:03 -0400

    -a
    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,6 @@
-// $(document).ready(function() {
+$(document).ready(function() {
+	$('.slider').each(function( index ) {
+	$(this).append('<div class="slideroverlay"></div>')
+	});
+});
 
-// 	$('#moreabout').click(function() {
-// 		event.preventDefault();
-// 		$('#overlay').addClass('show');
-// 		$('#facts').addClass('show');
-// 		$('.projects').addClass('hide');
-// 	});
-// 	$('#close').click(function() {
-// 		event.preventDefault();
-// 			$('#overlay').removeClass('show');
-// 			$('.projects').removeClass('hide');
-// 			$('#facts').removeClass('show');
-// 	});
-// });
