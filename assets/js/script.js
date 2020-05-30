18974f715c81b78ec8e6c6106e13ad86e9c9c2af
Tracy Ma <tma523@gmail.com>
2020-05-30 15:15:08 -0400

    last bits:

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,26 @@ $(document).ready(function() {
 	$('.slider').each(function( index ) {
 	$(this).append('<div class="slideroverlay"></div>')
 	});
-});
 
+
+
+
+	$( ".btn-simple" ).each(function(index) {
+
+		$(this).addClass('off');
+
+    	$(this).find('.clickarea').on("mousedown", function(){
+    		$(this).parent().toggleClass('pressed');
+    	});
+    	$(this).find('.clickarea').on("mouseup", function(){
+    		$(this).parent().toggleClass('pressed');
+		});
+
+    		
+
+
+    	
+	});
+
+    	
+});
